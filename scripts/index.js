129db61a04bbaa67520ef4f08c24c2c4c9b431b0
ankita067 <45871129+ankita067@users.noreply.github.com>
2020-02-24 16:32:07 +0530

    Create and All Post Button
    Created the all post button and create post button

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+var modal2 = document.getElementById("mymodal2");
+var btn2 = document.getElementById("mybtn2");
+var span2 = document.getElementsByClassName("close2")[0];
+btn2.onclick = function () {
+    modal2.style.display = "block";
+}
+span2.onclick = function () {
+    modal2.style.display = "none";
+}
